8f007fe05518f056f07cef1019719f5e47806063
Nithish <nithish.n.90@gmail.com>
2022-10-13 12:24:45 +0530

    Add files via upload

---
+++ Javascript.Day5.Functions.js
@@ -0,0 +1,23 @@
+// ---------------Print odd numbers in an array
+
+// let inputData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sortOddNumbers = (numbers) => {
+//   let oddNumbers = numbers.filter((number) => number % 2);
+//   console.log(oddNumbers);
+// };
+// console.log(sortOddNumbers(inputData));
+
+// ------------Convert all the strings to title caps in a string array
+
+// let msg = "Convert all the strings to title caps in a string array";
+
+// function titleCase(msg) {
+//   let step1 = msg.split(" ");
+//   let step2 = step1.map(
+//     (word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+//   );
+//   let step3 = step2.join(" ");
+//   console.log(step3);
+// }
+
+// titleCase(msg);
